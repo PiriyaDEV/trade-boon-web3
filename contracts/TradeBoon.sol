25d1c55a4598d79a92266ba81a4d421e1d427c16
Phumiphat <harryphat42@gmail.com>
2022-05-30 23:54:29 +0700

    Add comment in TradeBoon contract
    -Add more temples

--- contracts/TradeBoon.sol
+++ contracts/TradeBoon.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.0;
 
 contract TradeBoon {
+  /* define structure for temple */
   struct Temple {
     string name;
     string picture;
@@ -10,6 +11,7 @@ contract TradeBoon {
     uint256 totalAmount;
   }
 
+  /* define an event interface */
   event AddedTemple(uint256 tid, uint256 timestamp);
   event createTransaction(uint256 tid, uint256 amount, uint256 timestamp);
   
@@ -17,10 +19,12 @@ contract TradeBoon {
   mapping(address => uint256) public payment;
   mapping(uint256 => Temple) temples;
 
+  /* set contract owner's address */
   constructor() public {
     owner = msg.sender;
   }
 
+  /* this function adds a new temple to temple array */
   function addTemple(
     uint256 _tid,
     string memory _name,
@@ -38,9 +42,12 @@ contract TradeBoon {
         rating: _rating,
         totalAmount: 0
     });
+
+    /* emit add temple event */
     emit AddedTemple(_tid, _timestamp);
   }
 
+  /* this function return a temple data by id */
   function getTemple(uint256 _tid) public view returns (
     string memory name,
     string memory picture,
@@ -61,6 +68,7 @@ contract TradeBoon {
       );
   }
 
+  /* this function makes a transaction to contract onwer and updates temple total received */
   function makeMerit(uint256 _tid, uint256 _amount, uint256 _timestamp) public payable  {
     require(msg.value == _amount && msg.value > 0);
 
@@ -69,9 +77,11 @@ contract TradeBoon {
     Temple storage temple = temples[_tid];
     temple.totalAmount += msg.value;
 
+    /* emit create transaction event */
     emit createTransaction(_tid, msg.value, _timestamp);
   }
 
+  /* this function return contract owner's current balance */
   function getBalance() public view returns (uint256) {
     return address(this).balance;
   }
